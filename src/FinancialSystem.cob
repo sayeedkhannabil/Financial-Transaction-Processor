9ed6fcee3c8822647e7ff35c8e12bc959bd41c6d
sayeedkhannabil <sayeedkhannabil@gmail.com>
2023-09-27 20:49:22 -0500

    CREATE-ACCOUNT implemented

--- src/FinancialSystem.cob
+++ src/FinancialSystem.cob
@@ -10,6 +10,31 @@
        01 NUM-OF-TRANSACTIONS        PIC 9(4) VALUE 0.
        
        PROCEDURE DIVISION.
-           DISPLAY "Financial System Initialized."
+       CREATE-ACCOUNT.
+           DISPLAY "------ Create New Account ------"
+           
+           DISPLAY "Enter Account ID: "
+           ACCEPT ACCOUNT-ID OF ACCOUNT-RECORD
+           
+           DISPLAY "Enter Account Name: "
+           ACCEPT ACCOUNT-NAME
+           
+           DISPLAY "Enter Account Type: "
+           ACCEPT ACCOUNT-TYPE
+           
+           DISPLAY "Enter Initial Balance: "
+           ACCEPT ACCOUNT-BALANCE
+           
+           DISPLAY "Enter Account Status (Active/Inactive): "
+           ACCEPT ACCOUNT-STATUS
+           
+           DISPLAY "Enter Account Creation Date (YYYYMMDD): "
+           ACCEPT ACCOUNT-CREATION-DATE
+       
+           DISPLAY "Account successfully created."
+           
+           ADD 1 TO NUM-OF-ACCOUNTS
+           DISPLAY "Total number of accounts: " NUM-OF-ACCOUNTS
+
        
            STOP RUN.
